4b08ae1385d69719a609cc69d5ae09e83034861d
Alexander Ponomarenko <oleksandr.ponomarenko1@ibm.cim>
2021-03-26 04:36:51 +0200

    Update pipelines.py

--- videocard_parsing/pipelines.py
+++ videocard_parsing/pipelines.py
@@ -5,7 +5,7 @@ from scrapy.exceptions import DropItem
 from videocard_parsing.helpers import is_available, is_cheaper_then_max
 
 
-class VideocardParsingPipeline:
+class VideocardLocalParsingPipeline:
     def open_spider(self, spider):
         self.file = open('loaded/cards_rozetka.jl', 'w')
 
@@ -18,6 +18,16 @@ class VideocardParsingPipeline:
             if is_cheaper_then_max(adapter.get('price')) and is_available(adapter.get('availability')):
                 line = json.dumps(ItemAdapter(item).asdict()) + "\n"
                 self.file.write(line)
+                return item
+        else:
+            raise DropItem(f"Missing price in {item}")
+
+
+class VideocardCloudParsingPipeline:
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        if adapter.get('price'):
+            if is_cheaper_then_max(adapter.get('price')) and is_available(adapter.get('availability')):
                 return item
         else:
             raise DropItem(f"Missing price in {item}")
--- videocard_parsing/settings.py
+++ videocard_parsing/settings.py
@@ -63,7 +63,8 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'videocard_parsing.pipelines.VideocardParsingPipeline': 300,
+   # 'videocard_parsing.pipelines.VideocardLocalParsingPipeline': 300,
+   'videocard_parsing.pipelines.VideocardCloudParsingPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
